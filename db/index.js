3171bc2ed768b4734ae9dffc72d7112b6e7f641d
cferdinand <chrisferdinand07@gmail.com>
2020-04-03 03:50:17 -0400

    modified db pool to allow ssl connections

--- db/index.js
+++ db/index.js
@@ -1,11 +1,17 @@
 const Pool = require("pg").Pool;
 
-const pool = new Pool({
+const config = {
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   port: process.env.PGPORT
-});
+};
+
+if (process.env.NODE_ENV === "production") {
+  config.ssl = JSON.parse(process.env.SSL);
+}
+
+const pool = new Pool(config);
 
 pool.on("error", (err, client) => {
   console.error("Unexpected error on idle client", err);
